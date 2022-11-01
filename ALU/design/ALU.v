f0b1b872c59153ee2fb4833ed73220a1b73ca27d
Viraja Shivbhakta <shivbhak@viterbi-scf2.usc.edu>
2022-11-01 09:07:54 -0700

    changed adder module instant names after Ru changed

--- ALU/design/ALU.v
+++ ALU/design/ALU.v
@@ -4,12 +4,11 @@
 //`include "./include/sim_ver/DW02_mult.v"
 //`include "./include/sim_ver/DW_div.v"
 
-<<<<<<< HEAD
+
 `include "./include/sim_ver/DW01_add.v"
 
 
-=======
->>>>>>> 37a86f5ee83a1698f7f0c85b81a1f10da7fb07c0
+
 module ALU(rA_64bit_val, rB_64bit_val, R_ins, Op_code, WW, ALU_out);
 
 input [0:63] rA_64bit_val, rB_64bit_val;
@@ -23,13 +22,9 @@ parameter   VAND = 6'b000001, VOR =  6'b000010, VXOR = 6'b000011, VNOT = 6'b0001
 			VSRL = 6'b001011, VSRA = 6'b001100, VRTTH = 6'b001101, VDIV = 6'b001110, VMOD = 6'b001111,
 			VSQEU = 6'b010000, VSQOU = 6'b010001, VSQRT = 6'b010010, VNOP = 6'b000000;
 			
-<<<<<<< HEAD
+
 
 parameter R_ALU = 6'b101010, LOAD = 6'b100000, STORE = 6'b100001, BRANCH_EZ = 6'b100010, BRANCH_NZ = 6'b100011, NOP = 6'b111100;	//Type of instruction depending on INSTR[0:5] bits Op_code
-=======
-parameter R_ALU = 6'b101010, LOAD = 6'b100000, STORE = 6'b100001, BRANCH_EZ = 6'b100010; BRANCH_NZ = 6'b100011, NOP = 6'b111100;	//Type of instruction depending on INSTR[0:5] bits Op_code
->>>>>>> 37a86f5ee83a1698f7f0c85b81a1f10da7fb07c0
-
 parameter Width_8 = 2'b00, Width_16 = 2'b01, Width_32 = 2'b10, Width_64 = 2'b11;  //Width depending on WW
 
 //For 8bit adders
@@ -54,13 +49,13 @@ wire [0:6] carry_out;
 
 //8bit adders
 DW01_add #(8) add1 (.A(rA_64bit_val[0:7]), .B(rB_64bit_val[0:7]), .CI(carry_in[0]), .SUM(ALU_out[0:7]), .CO()); //MSB
-DW01_add #(8) add1 (.A(rA_64bit_val[8:15]), .B(rB_64bit_val[8:15]), .CI(carry_in[1]), .SUM(ALU_out[8:15]), .CO(carry_out[0]));
-DW01_add #(8) add1 (.A(rA_64bit_val[16:23]), .B(rB_64bit_val[16:23]), .CI(carry_in[2]), .SUM(ALU_out[16:23]), .CO(carry_out[1]));
-DW01_add #(8) add1 (.A(rA_64bit_val[24:31]), .B(rB_64bit_val[24:31]), .CI(carry_in[3]), .SUM(ALU_out[24:31]), .CO(carry_out[2]));
-DW01_add #(8) add1 (.A(rA_64bit_val[32:39]), .B(rB_64bit_val[32:39]), .CI(carry_in[4]), .SUM(ALU_out[32:39]), .CO(carry_out[3]));
-DW01_add #(8) add1 (.A(rA_64bit_val[40:47]), .B(rB_64bit_val[40:47]), .CI(carry_in[5]), .SUM(ALU_out[40:47]), .CO(carry_out[4]));
-DW01_add #(8) add1 (.A(rA_64bit_val[48:55]), .B(rB_64bit_val[48:55]), .CI(carry_in[6]), .SUM(ALU_out[48:55]), .CO(carry_out[5]));
-DW01_add #(8) add1 (.A(rA_64bit_val[56:63]), .B(rB_64bit_val[56:63]), .CI(0), .SUM(ALU_out[56:63]), .CO(carry_out[6])); //LSB
+DW01_add #(8) add2 (.A(rA_64bit_val[8:15]), .B(rB_64bit_val[8:15]), .CI(carry_in[1]), .SUM(ALU_out[8:15]), .CO(carry_out[0]));
+DW01_add #(8) add3 (.A(rA_64bit_val[16:23]), .B(rB_64bit_val[16:23]), .CI(carry_in[2]), .SUM(ALU_out[16:23]), .CO(carry_out[1]));
+DW01_add #(8) add4 (.A(rA_64bit_val[24:31]), .B(rB_64bit_val[24:31]), .CI(carry_in[3]), .SUM(ALU_out[24:31]), .CO(carry_out[2]));
+DW01_add #(8) add5 (.A(rA_64bit_val[32:39]), .B(rB_64bit_val[32:39]), .CI(carry_in[4]), .SUM(ALU_out[32:39]), .CO(carry_out[3]));
+DW01_add #(8) add6 (.A(rA_64bit_val[40:47]), .B(rB_64bit_val[40:47]), .CI(carry_in[5]), .SUM(ALU_out[40:47]), .CO(carry_out[4]));
+DW01_add #(8) add7 (.A(rA_64bit_val[48:55]), .B(rB_64bit_val[48:55]), .CI(carry_in[6]), .SUM(ALU_out[48:55]), .CO(carry_out[5]));
+DW01_add #(8) add8 (.A(rA_64bit_val[56:63]), .B(rB_64bit_val[56:63]), .CI(0), .SUM(ALU_out[56:63]), .CO(carry_out[6])); //LSB
 
 // //8 8 bit adders
 // DW01_add #(8) dwad1(.rA_8bit_p1,rB_8bit_p1 ,0,ALU_8bit_p1,CO);	
@@ -86,7 +81,7 @@ DW01_add #(8) add1 (.A(rA_64bit_val[56:63]), .B(rB_64bit_val[56:63]), .CI(0), .S
 // DW01_add #(64) dwad15(rA_64bit_p1,rB_64bit_p1 ,0,ALU_64bit_p1,CO);
 
 always @(*) begin		
-<<<<<<< HEAD
+
 
 	//case(ID_EX_Instr[0:5])
 	case(Op_code) 
@@ -201,7 +196,7 @@ always @(*) begin
 									
 		end
 	endcase
-=======
+
 //case(ID_EX_Instr[0:5])
 case(Op_code)
 	R_ALU: begin
@@ -274,7 +269,7 @@ case(Op_code)
 	end
 endcase
 
->>>>>>> 37a86f5ee83a1698f7f0c85b81a1f10da7fb07c0
+
 end
 
 endmodule
